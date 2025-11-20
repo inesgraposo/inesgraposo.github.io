72fc9da51179198f0722b3964480ca950c218b73
inesgraposo <50418080+inesgraposo@users.noreply.github.com>
2025-11-20 15:04:48 +0100

    Update research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const abstractToggles = document.querySelectorAll('.abstract-toggle');
+
+  abstractToggles.forEach(toggle => {
+    toggle.addEventListener('click', (event) => {
+      event.preventDefault();
+      const dropdown = toggle.parentElement.nextElementSibling;
+      dropdown.classList.toggle('open');
+    });
+  });
+});
 
